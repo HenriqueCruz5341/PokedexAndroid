75529e7b5c065447d7c0769a59e34036d792a036
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2023-02-01 17:27:45 -0300

    Comment all region related API models

--- app/src/main/java/com/example/pokedex/repository/api/model/region/LocationAreaDto.kt
+++ app/src/main/java/com/example/pokedex/repository/api/model/region/LocationAreaDto.kt
@@ -4,6 +4,15 @@ import com.example.pokedex.repository.api.model.PageableItemDto
 import com.example.pokedex.repository.api.model.pokemonSpecie.PokemonBasicDto
 import com.google.gson.annotations.SerializedName
 
+/**
+ * The information when requesting a location area.
+ *
+ * This class is used to parse the JSON response from the API when requesting a location area.
+ *
+ * @property encounters The list of pokemon encounters on this location area.
+ * @property id The location area unique identifier.
+ * @property name The location area name.
+ */
 class LocationAreaDto {
     @SerializedName("pokemon_encounters")
     var encounters: List<EncounterDto> = listOf()
--- app/src/main/java/com/example/pokedex/repository/api/model/region/RegionDto.kt
+++ app/src/main/java/com/example/pokedex/repository/api/model/region/RegionDto.kt
@@ -3,6 +3,15 @@ package com.example.pokedex.repository.api.model.region
 import com.example.pokedex.repository.api.model.PageableItemDto
 import com.google.gson.annotations.SerializedName
 
+/**
+ * The information when requesting a region.
+ *
+ * This class is used to parse the JSON response from the API when requesting a region.
+ *
+ * @property locations The list of locations on this region.
+ * @property id The region unique identifier.
+ * @property name The region name.
+ */
 class RegionDto {
     @SerializedName("locations")
     var locations: List<PageableItemDto> = listOf()
